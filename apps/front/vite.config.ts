15ba5a3b74b2e9b0669fd4969281b64663856661
amazzat <augustintherome@gmail.com>
2022-03-30 16:44:05 +0600

    chore: cleaned mess

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_TRPC_URL as string,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
